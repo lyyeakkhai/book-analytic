a1bbc0e03cacdbc155a52614b8930bea03f9dc75
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2026-03-24 17:12:51 +0700

    Implement address module

--- address-book-api/src/app.module.ts
+++ address-book-api/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AddressModule } from './address/address.module';
+import { AddressBookModule } from './address-book/address-book.module';
+import { AddressBookController } from './address-book.controller';
+import { AddressBookController } from './address-book.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AddressModule, AddressBookModule],
+  controllers: [AppController, AddressBookController],
   providers: [AppService],
 })
 export class AppModule {}
